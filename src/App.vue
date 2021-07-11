b1ad6a270aae635fa6896dbba47e039fed3aa1dc
Connor <connorm11111@gmail.com>
2021-07-11 14:15:30 +0200

    lesson2

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,59 @@
 <template>
   <v-app>
+    <v-app-bar app color="primary" dark>
+      <v-toolbar-title>Vuetify Dashboard </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn text rounded>Home</v-btn>
+      <v-btn text rounded>Login</v-btn>
+    </v-app-bar>
+    <v-content>
+      <!-- Login Module -->
+      <v-card width="400" class="mx-auto mt-5">
+        <v-card-title>
+          <h1 class="diplay-1">Login</h1>
+        </v-card-title>
+        <v-card-text>
+          <v-form>
+            <v-text-field label="Username" prepend-icon="mdi-account-circle" />
+            <v-text-field
+              :type="showPassword ? 'tex t' : 'password'"
+              label="Password"
+              prepend-icon="mdi-lock"
+              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+              @click:append="showPassword = !showPassword"
+            />
+          </v-form>
+        </v-card-text>
 
-  <v-card width="400" class="mx-auto mt-5">
-    <v-card-title>
-        <h1 class="diplay-1">Login</h1>
-    </v-card-title>
-  <v-card-text>
-    <v-form>
-      <v-text-field 
-      label="Username"
-      prepend-icon="mdi-account-circle"
-       />
-      <v-text-field
-       :type="showPassword ? 'tex t' : 'password'  "
-       label="Password" 
-       prepend-icon="mdi-lock"
-       :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-       @click:append="showPassword = !showPassword"
-       />
-    </v-form>  
-  </v-card-text>
+        <v-card-actions>
+          <v-btn color="success"> Register </v-btn>
+          <v-btn color="info"> Login </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-content>
 
-  <v-card-actions>
-    <v-btn
-      color="success"
-    >
-      Register
-    </v-btn>
-    <v-btn
-      color="info"
-    >
-      Login 
-    </v-btn> 
-  </v-card-actions>
-
-  </v-card>
- 
-
+    <v-footer color="primary lighten-1" padless>
+      <v-row justify="center" no-gutters>
+        <v-btn
+          v-for="link in links"
+          :key="link"
+          color="white"
+          text
+          rounded
+          class="my-2"
+        >
+          {{ link }}
+        </v-btn>
+        <v-col class="primary lighten-2 py-4 text-center white--text" cols="12">
+          {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+        </v-col>
+      </v-row>
+    </v-footer>
   </v-app>
+  <!-- login module end -->
 </template>
 
 <script>
-
 // import HelloWorld from './components/HelloWorld.vue';
 
 export default {
@@ -49,11 +61,14 @@ export default {
   // components : {
   //   HelloWorld
 
- 
   // },
   data: () => ({
     //
-    showPassword: false
+    showPassword: false,
+    links: [
+      'Home',
+      'Login'
+    ]
   }),
 };
 </script>
